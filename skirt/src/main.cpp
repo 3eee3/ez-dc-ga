3f88e8c36552728c99d7a442c56eb992eab460e5
Gerhard Aigner <gerhard.aigner@uibk.ac.at>
2015-12-29 00:06:03 +0100

    Verry first commit with a hello World message ;-)

---
+++ skirt/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 28, 2015
+ *      Author: littlelion
+ */
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main (int argv, char* argc[]) {
+	// TODO the program
+
+	cout << "blaaa..." << endl;
+
+	return EXIT_SUCCESS;
+}
+
+
+
