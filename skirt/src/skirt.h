37ef558d88e60f6ee3aa2b9cedaaf8dfdb717d2d
Gerhard Aigner <gerhard.aigner@uibk.ac.at>
2015-12-29 05:01:24 +0100

    generated skirt model files.

---
+++ skirt/src/skirt.h
@@ -0,0 +1,14 @@
+// This is a .h file for the model: skirt
+// Don't edit! This is an auto-generated file by blender2oGL. Modifications are not permanent.
+
+// Positions: 3456
+// Texels: 0
+// Normals: 40
+// Faces: 6144
+// Vertices: 18432
+
+const int skirtVertices;
+const float skirtPositions[55296];
+const float skirtTexels[36864];
+const float skirtNormals[55296];
+
