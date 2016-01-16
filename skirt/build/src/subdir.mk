1fed09cedcfb4e9d6bb0e4efc1cf03b872b233e0
Gerhard Aigner <gerhard.aigner@uibk.ac.at>
2016-01-16 08:50:15 +0100

    build folder with makefiles (auto-generated by eclipse) If You don't use eclipse cd to the build folder and call make to build the project.

---
+++ skirt/build/src/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/main.cpp 
+
+C_SRCS += \
+../src/skirt.c 
+
+OBJS += \
+./src/main.o \
+./src/skirt.o 
+
+C_DEPS += \
+./src/skirt.d 
+
+CPP_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
